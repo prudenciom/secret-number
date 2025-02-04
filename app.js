5cf6c20bbdd2079fbbeeed3c671de197b1b62c50
Maiara Franco <maiarafrancoprudencio@gmail.com>
2025-02-04 13:05:52 -0300

    alteracao no titulo

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function exibirTexto(tag, texto) {
 
 function mensagemInicial() {
     exibirTexto('h1', 'Jogo do número secreto');
-    exibirTexto('p', 'Escolha um número entre 1 e 100');
+    exibirTexto('p', 'Escolha um número entre 1 e 50');
 }
   mensagemInicial();
 
